44a7df137b64839aafb69f7df766c43e5385f0eb
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-08-25 00:26:13 +0500

    Update Question1.cpp

--- LAB 1/Question1.cpp
+++ LAB 1/Question1.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+	private:
+	    double balance;
+	
+	public:
+	    
+	    BankAccount() {
+	        balance = 0.0;
+	    }
+	
+	    BankAccount(double initialBalance) {
+	        balance = initialBalance;
+	    }
+	
+	    BankAccount(const BankAccount &acc) {
+	        balance = acc.balance;
+	    }
+
+	    void deposit(double amount) {
+	        balance += amount;
+	    }
+	 
+	    void withdraw(double amount) {
+	        if (amount <= balance) {
+	            balance -= amount;
+	        } else {
+	            cout << "Amount can't be withdrawn (Insufficient Funds) !" << endl;
+	        }
+	    }
+	
+	    double getBalance() const {
+	        return balance;
+	    }
+};
+
+
+int main() {
+    
+    BankAccount account1;
+    cout << "Balance of account1: $" << account1.getBalance() << endl;
+    BankAccount account2(1000.0);
+    cout << "Balance of account2: $" << account2.getBalance() << endl;
+    BankAccount account3(account2);  
+    account3.withdraw(200.0);
+    cout << "Balance of account3: $" << account3.getBalance() << endl;
+    cout << "Balance of account2: $" << account2.getBalance() << endl;
+
+    return 0;
+}
 
