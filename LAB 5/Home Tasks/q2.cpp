a00a75d0f33f01a61b626792b3e7a15a96b3ba13
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-10-02 18:41:05 +0500

    Update q2.cpp

--- LAB 5/Home Tasks/q2.cpp
+++ LAB 5/Home Tasks/q2.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
 using namespace std;
 
-void showNumbers(int x) {
-    if (x == 0)
-        return;
-    cout << x << " ";
-    showNumbers(x - 1);
+void printNumbers(int n) {
+    if (n <= 0) return;  
+    cout << n << " ";
+    printNumbers(n - 1);
 }
 
-void alpha(int x);
-void beta(int x);
 
-void alpha(int x) {
-    if (x <= 0)
-        return;
-    cout << "A: " << x << endl;
-    beta(x - 1);
+void functionB(int n);
+
+void functionA(int n) {
+    if (n <= 0) return;  
+    cout << n << " ";
+    functionB(n - 1);
 }
 
-void beta(int x) {
-    if (x <= 0)
-        return;
-    cout << "B: " << x << endl;
-    alpha(x - 1);
+void functionB(int n) {
+    if (n <= 0) return; 
+    cout << n << " ";
+    functionA(n - 1);
 }
 
 int main() {
-    int option, value;
-    cout << "1. Direct Recursion" << endl;
-    cout << "2. Indirect Recursion" << endl;
-    cout << "Enter choice: ";
-    cin >> option;
+    int n;
 
-    cout << "Enter number: ";
-    cin >> value;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Direct recursion output: ";
+    printNumbers(n);
+    cout << "\n\n";
 
-    if (option == 1)
-        showNumbers(value);
-    else if (option == 2)
-        alpha(value);
-    else
-        cout << "Invalid choice" << endl;
+    cout << "Indirect recursion output: ";
+    functionA(n);
+    cout << endl;
 
     return 0;
 }
